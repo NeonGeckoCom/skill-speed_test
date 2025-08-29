57ecc4f4725a2a40731177487258e6a6518369d0
Daniel McKnight <daniel@neon.ai>
2025-08-29 16:54:07 -0700

    Add license notice and auto-format files

--- test/test_skill.py
+++ test/test_skill.py
@@ -39,15 +39,17 @@ class TestSkillMethods(SkillTestCase):
     def test_handle_run_speed_test(self):
         on_notification_set = Mock()
         on_notification_clear = Mock()
-        self.skill.bus.on("ovos.notification.api.set.controlled",
-                          on_notification_set)
-        self.skill.bus.on("ovos.notification.api.remove.controlled",
-                          on_notification_clear)
+        self.skill.bus.on(
+            "ovos.notification.api.set.controlled", on_notification_set
+        )
+        self.skill.bus.on(
+            "ovos.notification.api.remove.controlled", on_notification_clear
+        )
         self.skill.handle_run_speed_test(Message("test"))
         self.skill.speak_dialog.assert_any_call("start_test")
         args = self.skill.speak_dialog.call_args
         self.assertEqual(args[0][0], "results")
-        self.assertEqual(set(args[0][1].keys()), {'down', 'up', 'ping'})
+        self.assertEqual(set(args[0][1].keys()), {"down", "up", "ping"})
         on_notification_set.assert_called_once()
         on_notification_clear.assert_called_once()
 
@@ -61,5 +63,6 @@ class TestSkillMethods(SkillTestCase):
         self.assertIsInstance(result.ping, float)
         self.assertLess(result.ping, 1000)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
